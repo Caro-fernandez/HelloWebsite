aa233b0801d20e59a38309a607617254f89fd464
unknown <mcarolinafernandez1@gmail.com>
2024-04-14 14:05:22 -0300

    html, css, img files added!

---
+++ hello.js
@@ -0,0 +1,9 @@
+const logo = document.getElementById('logo');
+const mainContainer  = document.getElementById('main-container');
+const levelButton = document.getElementById('level-button');
+const levels = document.getElementById('levels');
+const cursos = document.getElementById('cursos');
+const contacto = document.getElementById('contacto');
+
+
+
